c088f3a3324e25798f82413826149f0461d5261f
Justin Zollars <fogonthedowns@gmail.com>
2015-01-28 13:09:49 -0800

    tag

---
+++ slide/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  slide
+//
+//  Created by Justin Zollars on 1/28/15.
+//  Copyright (c) 2015 rmb. All rights reserved.
+//
+
+import UIKit
+
+class UserModel: NSObject {
+   
+}
