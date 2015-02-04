55ee4c2617b5291b6e7f09cfdd7b6b06eafc54ca
Justin Zollars <142996+fogonthedowns@users.noreply.github.com>
2015-02-03 22:43:54 -0800

    update

--- slide/HomeTableViewController.swift
+++ slide/HomeTableViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import QuartzCore
+import MediaPlayer
 
 // the protocol, api protocol is referenced by the class below
 // the method outlined is included in the class
@@ -16,11 +16,12 @@ protocol APIProtocol {
     func didReceiveResult(results: JSON)
 }
 
-class HomeTableViewController: UITableViewController, APIProtocol {
+class HomeTableViewController: UITableViewController, NSURLSessionDelegate, NSURLSessionTaskDelegate, NSURLSessionDownloadDelegate, APIProtocol {
     let userObject = UserModel()
     var latitude = "1"
     var longitute = "1"
     var videoModelList: NSMutableArray = [] // This is the array that my tableView
+    var sharedInstance = VideoDataToAPI.sharedInstance
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,20 +30,29 @@ class HomeTableViewController: UITableViewController, APIProtocol {
         self.tableView.rowHeight = 115.0
         
         self.title = "Soma"
-        // change navigation item title color
- 
-//        var homeButton : UIBarButtonItem = UIBarButtonItem(title: "Camera", style: UIBarButtonItemStyle.Plain, target: self, action: "")
-//        
-//        
-//        self.navigationItem.leftBarButtonItem = homeButton
-//        
+        // ----------------
+        // lots of code
+        
+        let longpress = UILongPressGestureRecognizer(target: self, action: "longPress:")
+        
+        tableView.addGestureRecognizer(longpress)
+        
+        struct Static {
+            static var session: NSURLSession?
+            static var token: dispatch_once_t = 0
+        }
+        
+        dispatch_once(&Static.token) {
+            let configuration = NSURLSessionConfiguration.backgroundSessionConfiguration(BackgroundSessionDownloadIdentifier)
+            Static.session = NSURLSession(configuration: configuration, delegate: self, delegateQueue: nil)
+        }
+        
+        self.session = Static.session;
+        
 
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -61,7 +71,7 @@ class HomeTableViewController: UITableViewController, APIProtocol {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        NSLog("Array Count = %u", videoModelList.count);
+        // NSLog("Array Count = %u", videoModelList.count);
         return videoModelList.count
     }
 
@@ -70,8 +80,9 @@ class HomeTableViewController: UITableViewController, APIProtocol {
         let cell = tableView.dequeueReusableCellWithIdentifier("VideoCell") as VideoCellTableViewCell
         let video: VideoModel = videoModelList[indexPath.row] as VideoModel
         cell.titleLabel.text = video.film
+        self.start(video.film)
         var urlString = "https://s3-us-west-1.amazonaws.com/slideby/" + video.img
-        NSLog("video url: %@", urlString)
+        // NSLog("video url: %@", urlString)
         let url = NSURL(string: urlString)
         let main_queue = dispatch_get_main_queue()
         
@@ -92,52 +103,47 @@ class HomeTableViewController: UITableViewController, APIProtocol {
         return cell
     }
     
-
+    func longPress(sender:UILongPressGestureRecognizer!) {
+        let longPress = sender as UILongPressGestureRecognizer
+        var locationInView = longPress.locationInView(tableView)
+        var indexPath = tableView.indexPathForRowAtPoint(locationInView)
+        var cell = self.tableView.cellForRowAtIndexPath(indexPath!) as VideoCellTableViewCell
+        var urlString = cell.titleLabel.text!
+        // NSLog(urlString)
+        let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        let documentsPath = paths.first as? String
+        let filePath = documentsPath! + "/" + cell.titleLabel.text!
+        
+        if (sender.state == UIGestureRecognizerState.Ended) {
+            println("Long press Ended");
+            println("Long press Ended%@", self.view.subviews.count);
+            self.moviePlayer.stop()
+            self.moviePlayer.view.removeFromSuperview()
+            self.tableView.reloadData()
+        } else if (sender.state == UIGestureRecognizerState.Began) {
+            println("Long press detected.");
+            let path = NSBundle.mainBundle().pathForResource("video", ofType:"m4v")
+            let url = NSURL.fileURLWithPath(filePath)
+            println(url)
+            self.moviePlayer = MPMoviePlayerController(contentURL: url)
+            if var player = self.moviePlayer {
+                player.view.frame = self.view.bounds
+                player.prepareToPlay()
+                player.scalingMode = .AspectFill
+                self.view.addSubview(player.view)
+            }
+        }
+    }
     
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    func longPressGestureRecognized(gestureRecognizer: UIGestureRecognizer) {
+        let longPress = gestureRecognizer as UILongPressGestureRecognizer
+        
+        let state = longPress.state
+        
+        var locationInView = longPress.locationInView(tableView)
+        
+        var indexPath = tableView.indexPathForRowAtPoint(locationInView)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     func didReceiveResult(result: JSON) {
         // local array var used in this function
@@ -163,5 +169,139 @@ class HomeTableViewController: UITableViewController, APIProtocol {
             self.tableView.reloadData()
         })
     }
+// ------------------------------------------
+// Lots of code
+    
+    @IBOutlet var progressView: UIProgressView!
+    @IBOutlet var statusLabel: UILabel!
 
+    var session: NSURLSession?
+    var downloadTask: NSURLSessionDownloadTask?
+    var moviePlayer:MPMoviePlayerController!
+
+
+    func start(s3downloadname: NSString) {
+         NSLog("************************************************%@", s3downloadname)
+        if (self.downloadTask != nil) {
+            // push current downloadtask into an array
+            // array processes recursive function
+            NSLog("------------------------------- return -----------------%@", s3downloadname)
+
+            return;
+        }
+        // test if file path is blank
+        // if so go forward
+        // if not return
+        
+        let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        let documentsPath = paths.first as? String
+        let filePath = documentsPath! + "/" + s3downloadname
+        NSLog("------------------------------- filePath -----------------%@", filePath)
+        let url = NSURL.fileURLWithPath(filePath)
+        
+        // if the file exists return, don't start an asynch download
+        if NSFileManager.defaultManager().fileExistsAtPath(filePath) {
+            NSLog("FILE ALREADY DOWNLOADED")
+            return;
+        }
+        
+        sharedInstance.downloadName = s3downloadname
+        let getPreSignedURLRequest = AWSS3GetPreSignedURLRequest()
+        getPreSignedURLRequest.bucket = S3BucketName
+        getPreSignedURLRequest.key = s3downloadname
+        getPreSignedURLRequest.HTTPMethod = AWSHTTPMethod.GET
+        getPreSignedURLRequest.expires = NSDate(timeIntervalSinceNow: 3600)
+        
+    
+        AWSS3PreSignedURLBuilder.defaultS3PreSignedURLBuilder().getPreSignedURL(getPreSignedURLRequest) .continueWithBlock { (task:BFTask!) -> (AnyObject!) in
+            if (task.error != nil) {
+                NSLog("Error: %@", task.error)
+            } else {
+                let presignedURL = task.result as NSURL!
+                if (presignedURL != nil) {
+                    NSLog("download presignedURL is: \n%@", presignedURL)
+                    
+                    let request = NSURLRequest(URL: presignedURL)
+                    self.downloadTask = self.session?.downloadTaskWithRequest(request)
+                    self.downloadTask?.resume()
+                }
+            }
+            return nil;
+        }
+    }
+
+    func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        
+        let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+        
+        NSLog("DownloadTask progress: %lf", progress)
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            //            self.progressView.progress = progress
+            //            self.statusLabel.text = "Downloading..."
+        }
+        
+    }
+
+
+    func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didFinishDownloadingToURL location: NSURL) {
+        
+//        NSLog("[%@ %@]", reflect(self).summary, __FUNCTION__)
+        
+        let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        
+        let documentsPath = paths.first as? String
+        let filePath = documentsPath! + "/" + sharedInstance.downloadName
+//        NSLog("the filepath let:%@", filePath)
+//        NSLog("the downloadkename let %@", sharedInstance.downloadName)
+        NSFileManager.defaultManager().moveItemAtURL(location, toURL: NSURL.fileURLWithPath(filePath)!, error: nil)
+        
+        
+        //update UI elements
+        dispatch_async(dispatch_get_main_queue()) {
+//            let path = NSBundle.mainBundle().pathForResource("video", ofType:"m4v")
+//            let url = NSURL.fileURLWithPath(filePath)
+//            println(url)
+//            self.moviePlayer = MPMoviePlayerController(contentURL: url)
+//            if let player = self.moviePlayer {
+//                player.view.frame = self.view.bounds
+//                player.prepareToPlay()
+//                player.scalingMode = .AspectFill
+//                self.view.addSubview(player.view)
+//            }
+        }
+    }
+
+
+    func URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError error: NSError?) {
+        if (error == nil) {
+//            dispatch_async(dispatch_get_main_queue()) {
+//                self.statusLabel.text = "Download Successfully"
+//            }
+            // NSLog("S3 DownloadTask: %@ completed successfully", task);
+        } else {
+            dispatch_async(dispatch_get_main_queue()) {
+//                self.statusLabel.text = "Download Failed"
+            }
+            NSLog("S3 DownloadTask: %@ completed with error: %@", task, error!.localizedDescription);
+        }
+        
+//        dispatch_async(dispatch_get_main_queue()) {
+//            self.progressView.progress = Float(task.countOfBytesReceived) / Float(task.countOfBytesExpectedToReceive)
+//        }
+        
+        self.downloadTask = nil
+    }
+
+    func URLSessionDidFinishEventsForBackgroundURLSession(session: NSURLSession) {
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        if ((appDelegate.backgroundDownloadSessionCompletionHandler) != nil) {
+            let completionHandler:() = appDelegate.backgroundDownloadSessionCompletionHandler!;
+            appDelegate.backgroundDownloadSessionCompletionHandler = nil
+            completionHandler
+        }
+        
+        // NSLog("Completion Handler has been invoked, background download task has finished.");
+    }
 }
