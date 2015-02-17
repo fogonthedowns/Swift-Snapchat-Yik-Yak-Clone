33472c9086d75a193a0162b6398a844799c70c63
Justin Zollars <fogonthedowns@gmail.com>
2015-02-17 10:14:36 -0800

    comments controllers framework

---
+++ slide/CommentsUINavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  CommentsUINavigationController.swift
+//  slide
+//
+//  Created by Justin Zollars on 2/17/15.
+//  Copyright (c) 2015 rmb. All rights reserved.
+//
+
+import UIKit
+
+class CommentsUINavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
