3659305fe7a5e3ddff9636e57b56beb26f4f36f2
Justin Zollars <142996+fogonthedowns@users.noreply.github.com>
2015-02-22 21:36:36 -0800

    add app icons

---
+++ slide/PrivacyViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PrivacyViewController.swift
+//  slide
+//
+//  Created by Justin Zollars on 2/22/15.
+//  Copyright (c) 2015 rmb. All rights reserved.
+//
+
+import UIKit
+
+class PrivacyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func goBack(sender: AnyObject) {
+      NSNotificationCenter.defaultCenter().postNotificationName(didClickToNavigateToDistricts, object: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
